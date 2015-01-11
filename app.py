a899734f1401e1ec48831fc116acd6ae274faeba
Shakna Israel <s4b3r6@gmail.com>
2015-01-11 22:27:33 +0000

    Switched to CherryPy Server

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-import SimpleHTTPServer
-import SocketServer
+import cherrypy
+import os.path
 
-PORT = 8080
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+class RootPage:
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+	def index(self):
+		raise cherrypy.HTTPRedirect("/static/index.html")
 
-print "Serving at port: ", PORT
-httpd.serve_forever()
+	def getMachineInfo(self):
+		return "hello"
+
+	index.exposed = True
+	getMachineInfo.exposed = True
+
+if __name__ == '__main__':
+	conf = {
+				'global': {
+					'server.socket_host': '0.0.0.0',
+					'server.socket_port': 8080,
+					'server.threaed_pool': 10
+				},
+
+				'/': {
+					'tools.staticdir.root': current_dir
+				},
+
+				'/static': {
+					'tools.staticdir.on': True,
+					'tools.staticdir.dir': 'static'
+				}
+
+			}
+	cherrypy.quickstart(RootPage(), config=conf)
